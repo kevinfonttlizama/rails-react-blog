d46631bc4069356d1b66502c6cc7a97628fe5221
Kevin <kevin.andresfontt@gmail.com>
2023-08-17 17:40:08 -0400

    Creating Crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,20 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(title: "...", body:"...")
-    
+
     if article.save
       redirect_to @article
-    
+
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
