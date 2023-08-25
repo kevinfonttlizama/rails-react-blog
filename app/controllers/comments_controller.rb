1117ea862a56babedb0499403fb398d974d6d27f
Kevin <kevin.andresfontt@gmail.com>
2023-08-25 09:39:39 -0400

    bug fix Commenter's no name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,8 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
-
   private
-
-  def comment_params
-    params.require(:comment).permit(:commenter, :body)
-  end
-
-
+    def comment_params
+      params.require(:comment).permit(:Commenter, :body)
+    end
 end
